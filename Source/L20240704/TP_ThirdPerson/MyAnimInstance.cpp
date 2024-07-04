e55625a66f4d2449045633837361857d8dba578d
junios <junios@naver.com>
2024-07-04 15:57:02 +0900

    BlendSpace 사용해보기

---
+++ Source/L20240704/TP_ThirdPerson/MyAnimInstance.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	ACharacter* Pawn = Cast<ACharacter>(TryGetPawnOwner());
+	if (IsValid(Pawn))
+	{
+		Speed = Pawn->GetCharacterMovement()->Velocity.Size2D();
+		//UE_LOG(LogTemp, Warning, TEXT("%f"), Speed);
+		bIsMoveingOnGround = Pawn->GetCharacterMovement()->IsMovingOnGround();
+		bIsDead = false;
+		
+		Direction = CalculateDirection(Pawn->GetCharacterMovement()->Velocity,
+			Pawn->GetActorRotation());
+	}
+}
---
+++ Source/L20240704/TP_ThirdPerson/MyAnimInstance.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20240704_API UMyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Status")
+	float Speed;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Status")
+	uint8 bIsMoveingOnGround : 1;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Status")
+	uint8 bIsDead : 1;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Status")
+	float Direction;
+
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+};
